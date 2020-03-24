e2f16227684d712a19bcf08a2e7394c593edda57
WhiteCherryAir <55917848+WhiteCherryAir@users.noreply.github.com>
2020-03-24 20:24:38 +0800

    Update CmfzApplication.java

--- src/main/java/com/ljy/CmfzApplication.java
+++ src/main/java/com/ljy/CmfzApplication.java
@@ -11,6 +11,7 @@ public class CmfzApplication {
     public static void main(String[] args) {
         SpringApplication.run(CmfzApplication.class, args);
         System.out.println();
+        System.out.println();
     }
 
 }
