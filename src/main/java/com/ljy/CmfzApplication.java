80787dded371168705189b14fc8d24d0939023d0
WhiteCherryAir <1140547299@qq.com>
2020-03-24 20:38:50 +0800

    sysodel222

--- src/main/java/com/ljy/CmfzApplication.java
+++ src/main/java/com/ljy/CmfzApplication.java
@@ -12,7 +12,7 @@ public class CmfzApplication {
         SpringApplication.run(CmfzApplication.class, args);
         System.out.println();
         System.out.println();
-        System.out.println("222");
+
     }
 
 }
